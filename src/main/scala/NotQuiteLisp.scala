2a080661b003cab6f26c3e2efd86bd5407b7fa26
Mike Gehard <mike.gehard@gmail.com>
2015-12-18 20:12:39 -0700

    Use foldLeft instead of mutable variable with map

--- src/main/scala/NotQuiteLisp.scala
+++ src/main/scala/NotQuiteLisp.scala
@@ -1,11 +1,10 @@
 object NotQuiteLisp {
-  def entersBasementAt(s: String) = s.map(valueFor).map {
-    // this is a little magical but I believe it has to do
-    // with closing over the mutable sum variable.
-    var sum = 0; value => {
-      sum += value; sum
+  def entersBasementAt(s: String) = s.map(valueFor).foldLeft(List[Int]())((acc, value) => {
+    acc match {
+      case Nil => acc :+ value
+      case _ => acc :+ (acc.last + value)
     }
-  }.indexOf(-1) + 1
+  }).indexOf(-1) + 1
 
   def floorFor(s: String) = s.map(valueFor).sum
 
