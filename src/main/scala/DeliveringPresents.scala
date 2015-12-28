5a449c870d5581891a87f431325a8958bb3e3fc5
Mike Gehard <mike.gehard@gmail.com>
2015-12-28 16:33:48 -0700

    Solve the problem with RoboSanta

--- src/main/scala/DeliveringPresents.scala
+++ src/main/scala/DeliveringPresents.scala
@@ -1,7 +1,30 @@
+import scala.collection.immutable.IndexedSeq
+
 object DeliveringPresents {
-  def uniqueHouses(travelPattern: String) = {
-    housesVisited((0, 0), travelPattern.map(offsetFor)).distinct.length
+  def uniqueHousesWithRoboSanta(travelPattern: String): Int = {
+    val santaPath = pathFor(offsets(travelPattern), evenIndex)
+    val roboSantaPath = pathFor(offsets(travelPattern), oddIndex)
+
+    val santaHousesVisited = housesVisited((0,0), santaPath)
+    val roboSantaHousesVisited = housesVisited((0,0), roboSantaPath)
+
+    uniqueHouses(santaHousesVisited ++ roboSantaHousesVisited)
   }
+
+  def uniqueHousesSolo(travelPattern: String): Int = {
+    uniqueHouses(housesVisited((0, 0), offsets(travelPattern)))
+  }
+
+  def uniqueHouses(houses: Seq[(Int, Int)]): Int = houses.distinct.length
+
+  def pathFor(path: IndexedSeq[(Int, Int)], predicate: Int => Boolean): IndexedSeq[(Int, Int)] = {
+    path.zipWithIndex.filter((element) => predicate(element._2)).map(_._1)
+  }
+
+  def offsets(travelPattern: String): IndexedSeq[(Int, Int)] = travelPattern.map(offsetFor)
+
+  def evenIndex(index: Int) = index % 2 == 0
+  def oddIndex(index: Int) = !evenIndex(index)
 
   def offsetFor(c: Char) = c match {
     case '^' => (0, 1)
